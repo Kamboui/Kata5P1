8c01cf639e753690cf5ae28bc3ee4e17efa5f863
Kamboui <39406361+Kamboui@users.noreply.github.com>
2019-01-05 20:48:35 +0000

    Tercera Version

--- src/kata5p1/Kata5P1.java
+++ src/kata5p1/Kata5P1.java
@@ -1,13 +1,20 @@
 package kata5p1;
 
+import java.util.List;
 import static kata5p1.CrearTabla.createNewTable;
+import static kata5p1.MailListReader.read;
 
 public class Kata5P1 {
 
     public static void main(String[] args) {
         //SelectApp app = new SelectApp();
         //app.selectAll();
-        createNewTable();
+        //createNewTable();
+        
+        String ruta = "email.txt";
+        List<String> lista = read(ruta);
+        InsertarDatos ins = new InsertarDatos(lista);
+        ins.insert();
     }
 
 }
